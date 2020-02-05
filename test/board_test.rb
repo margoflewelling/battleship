156027559c02b2ed1136fe3a1a344ca969f65851
Margo Flewelling <flewelling.margo@gmail.com>
2020-02-05 16:04:56 -0700

    Added valid_coordinate method

--- test/board_test.rb
+++ test/board_test.rb
@@ -20,4 +20,9 @@ class BoardTest < Minitest::Test
       assert_equal 16, @board.cells.length
     end
 
+    def test_if_coordinate_is_on_board
+      assert_equal true, @board.valid_coordinate("A1")
+      assert_equal false, @board.valid_coordinate("A5")
+    end
+
 end
