6f1fb92482b862aa97cd33438145c7960fb4707d
Margo Flewelling <flewelling.margo@gmail.com>
2020-02-05 15:50:55 -0700

    Initialize board with cells

--- test/board_test.rb
+++ test/board_test.rb
@@ -7,5 +7,17 @@ require './lib/board'
 
 
 class BoardTest < Minitest::Test
-  
+
+    def setup
+    @board = Board.new
+    end
+
+    def test_it_exists
+      assert_instance_of Board, @board
+    end
+
+    def test_cell_attribute
+      assert_equal 16, @board.cells.length
+    end
+
 end
