83ccbfbb0cbbfd4712020b98c62bc1ad26420113
Margo Flewelling <flewelling.margo@gmail.com>
2020-02-05 15:13:46 -0700

    Add board class and board test files

---
+++ test/board_test.rb
@@ -0,0 +1,11 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+require './lib/cell'
+require './lib/board'
+
+
+class BoardTest < Minitest::Test
+  
+end
