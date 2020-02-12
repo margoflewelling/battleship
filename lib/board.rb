86e279b96c5cf4dc67a7141f9cabf3c845e075fa
Margo Flewelling <flewelling.margo@gmail.com>
2020-02-12 14:36:58 -0700

    Add loop for if they want to play again

--- lib/board.rb
+++ lib/board.rb
@@ -13,7 +13,7 @@ class Board
       "D3" => Cell.new("D3"), "D4" => Cell.new("D4")}
   end
 
-  def valid_coordinate(coordinate)
+  def valid_coordinate?(coordinate)
     @cells.include?(coordinate)
   end
 
